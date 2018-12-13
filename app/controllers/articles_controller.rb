c4296b9d7e4d5cbd586958adc79d0ca9e77778b7
Muhammad Farag <muhammad.farag@shopify.com>
2018-12-12 20:38:19 -0500

    Save new articles with strong parameters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,14 @@ class ArticlesController < ApplicationController
   def new; end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_parameters)
+    @article.save
+    redirect_to @article
+  end
+
+  private
+
+  def article_parameters
+    params.require(:article).permit(:title, :text)
   end
 end
