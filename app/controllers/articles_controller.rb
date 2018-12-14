ec77b9ce14c14840d5680e86de8dd4037b7441ba
Muhammad Farag <muhammad.farag@shopify.com>
2018-12-13 19:44:20 -0500

    If article is invalid, re-display create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,8 +14,11 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_parameters)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   private
